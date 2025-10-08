e445d0124175db93ec603ffe358d379aab0a3515
ImYichus <s22120205@alumnos.itsur.edu.mx>
2025-10-07 19:06:24 -0600

    VERSION 3.0

--- app/src/main/java/com/jqleapa/appnotas/ui/theme/Color.kt
+++ app/src/main/java/com/jqleapa/appnotas/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.jqleapa.appnotas.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// --- Colores Originales (Purple Theme) ---
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// --- Nuevos Colores para el Tema Emerald ---
+val EmeraldPrimary = Color(0xFF00A36C) // Verde esmeralda brillante
+val EmeraldSecondary = Color(0xFF00754F) // Verde oscuro secundario
+val EmeraldTertiary = Color(0xFF83E8AD) // Color claro acento
+val DarkSurface = Color(0xFF121212) // Fondo oscuro para el tema oscuro
+
+// Tema Claro Emerald
+val EmeraldLightPrimary = Color(0xFF00965C)
+val EmeraldLightSecondary = Color(0xFF388E3C)
+val EmeraldLightTertiary = Color(0xFF9CCC65)
+val EmeraldLightOnSurface = Color(0xFF1C1C1C)
